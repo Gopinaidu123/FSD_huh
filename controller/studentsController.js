241af3f86065b9d3e4519a7c2401c26a4a9b6f10
Gopinaidu123 <gopinaidu043@gmail.com>
2025-12-23 20:44:39 +0530

    feat: implement URL encoding and decoding utilities
    - Added `urlencoded.js` for parsing and serializing URL-encoded data.
    - Introduced `parseUrlencoded` and `serializeUrlencoded` functions.
    - Updated `utils.js` with utility functions for object handling and property management.
    - Updated `package.json` to version 14.2.0 and added necessary dependencies.
    - Created `webidl2js-wrapper.js` to export URL and URLSearchParams modules.

--- controller/studentsController.js
+++ controller/studentsController.js
@@ -1,28 +1,19 @@
-// const getStudents = (req , res) => {
+import student from "../models/studentsModels.js";
 
-//     let stdData = {
-//         name : "Gopi",
-//         rol : "4255"
-//     };
-//     res.status(200).json({data:stdData});
-// };
-
-// const addStudents = (req , res) => {
-    
-//     res.status(201).json({message : "Student data added successfully wow"
-//     });
-// }
-// export { getStudents , addStudents};
-
-const getStudents= (req,res) =>{
-    let stdData= 
-    {
-        "name" : "sobha",
-        "Roll" :"4235"
-    };
-    res.status(200).json({data : stdData});
+const getStudentsDetails = (req, res) => {
+    const mydata = {name: "thub", roll: "1234"}; //db
+    res.send(mydata);
 };
-const addstudents=(req ,res) => {
-    res.status(201) .json ({message : "data added" });
-}
-export {getStudents , addstudents};
+const addStudents = async(req, res) => {
+    try{
+         const data = req.body;
+    console.log(data);
+    // const addeddata = await student.create(data);
+    const addeddata = await student.insertMany(data);
+    console.log(addeddata);
+    res.status(201).json("data added");
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+};
+export {getStudentsDetails, addStudents};
